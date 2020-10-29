f074c5be5798795a461e0913477cfdd1a52d0704
Cahya Eka Permana <43898558+cahyaekapermana@users.noreply.github.com>
2020-10-30 02:29:35 +0700

    Update db_rentalps.sql

--- DATABASE/db_rentalps.sql
+++ DATABASE/db_rentalps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2020 at 10:57 AM
+-- Generation Time: Oct 29, 2020 at 08:26 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 5.6.40
 
@@ -25,27 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `tb_user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `tb_user` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `nama_lengkap` varchar(50) NOT NULL,
   `alamat` varchar(50) NOT NULL,
-  `no_telp` varchar(20) NOT NULL
+  `no_telp` varchar(20) NOT NULL,
+  `level` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `username`, `password`, `email`, `nama_lengkap`, `alamat`, `no_telp`, `level`, `created_at`) VALUES
+(1, 'cahya', 'cahya', '', '', '', '', 'User', '2020-10-29 12:05:10');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Indexes for table `tb_user`
 --
-ALTER TABLE `user`
+ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -53,10 +62,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `tb_user`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
