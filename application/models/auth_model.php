95293974aa748a2d6698802daf2579ac5a358886
argardiansyach <61655921+argardiansyach@users.noreply.github.com>
2020-11-16 12:28:57 +0700

    membuat login auth

---
+++ application/models/auth_model.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth_model extends CI_Model {
+
+	public function checkemail($email)
+    {
+        $this->db->where('email', $email);
+        return $this->db->get('auth');
+    }
+
+	public function regis($insert)
+	{
+		return $this->db->insert('auth', $insert);
+	}
+	
+	public function login($user, $password)
+	{
+		$this->db->where('username', $user);
+		$this->db->where('password', $password);
+        return $this->db->get('auth');
+	}
+
+}
+
+/* End of file auth_model.php */
+/* Location: ./application/models/auth_model.php */
