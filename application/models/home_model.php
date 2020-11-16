7f43de85bc29843805d3f723399084791116a37f
argardiansyach <61655921+argardiansyach@users.noreply.github.com>
2020-11-16 12:30:20 +0700

    membuat home model

---
+++ application/models/home_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home_model extends CI_Model {
+
+	public function getPcg()
+	{
+		return $this->db->get('packages_travel', 6);
+	}
+
+	public function booking($ins)
+	{
+		$this->db->insert('booking', $ins);
+	}
+}
+
+/* End of file home_model.php */
+/* Location: ./application/models/home_model.php */
